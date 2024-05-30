228867d80565604c82ab7c72d46116543f8b8b5a
Saurav-Bix <119658356+Saurav-Bix@users.noreply.github.com>
2024-05-30 14:35:48 +0200

    test thing
    ai cars

--- Nascar/Assets/Scripts/WaypointMover.cs
+++ Nascar/Assets/Scripts/WaypointMover.cs
@@ -8,11 +8,18 @@ public class WaypointMover : MonoBehaviour
     [SerializeField] private float moveSpeed = 5f;
     [SerializeField] private float distanceThreshold=0.1f;
     private Transform currentWaypoint;
+
+    public int max_lap = 3;
+    public int curr_lap = 0;
+    public Transform waypoint_checker;
+
     // Start is called before the first frame update
     void Start()
     {
+        
         currentWaypoint=waypoints.GetNextWaypoint(currentWaypoint);
         transform.position=currentWaypoint.position;
+        
 
         currentWaypoint=waypoints.GetNextWaypoint(currentWaypoint);
         transform.LookAt(currentWaypoint);
@@ -22,13 +29,48 @@ public class WaypointMover : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position=Vector3.MoveTowards(transform.position,currentWaypoint.position,moveSpeed*Time.deltaTime);
-        if(Vector3.Distance(transform.position,currentWaypoint.position)<distanceThreshold)
-        {
-             currentWaypoint=waypoints.GetNextWaypoint(currentWaypoint);
-             transform.LookAt(currentWaypoint);
+        if (curr_lap >= max_lap) return; // Stop moving after completing 3 loops
 
-        } 
-        
+        MoveTowardsWaypoint();
+        CheckIfWaypointReached();
     }
+
+     
+
+     private void MoveTowardsWaypoint()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, currentWaypoint.position, moveSpeed * Time.deltaTime);
+    }
+
+     private void CheckIfWaypointReached()
+    {
+        if (Vector3.Distance(transform.position, currentWaypoint.position) < distanceThreshold)
+        {
+            currentWaypoint = waypoints.GetNextWaypoint(currentWaypoint);
+            if (currentWaypoint == null)
+            {
+                curr_lap++;
+                if (curr_lap < max_lap)
+                {
+                    Start(); // Restart waypoints
+                }
+                else
+                {
+                    // Optionally handle what to do when maxLoops is reached
+                    Debug.Log("Completed all loops.");
+                }
+            }
+            else
+            {
+                transform.LookAt(currentWaypoint);
+            }
+        }
+    }
+
+
+
+
+
+
+    
 }
